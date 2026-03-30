5da84a65de0bcd52dd5af2f9d779176764dfc968
Sanskar <kumarsanskar816@gmail.com>
2026-03-31 03:03:12 +0530

    Added requirements for deployment

--- app.py
+++ app.py
@@ -105,6 +105,5 @@ def download():
 
     return send_file("report.csv", as_attachment=True)
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=10000)
